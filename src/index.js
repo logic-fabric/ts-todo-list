0eecbc13879c6132b8155a7a38fef979d0040926
logic-fabric <lowic@logic-fabric.fr>
2022-06-11 11:43:00 +0200

    refactor: convert reportWebVitals to TS

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import { App } from "./App.tsx";
 
 import "./index.css";
 
-import reportWebVitals from "./reportWebVitals";
+import { reportWebVitals } from "./reportWebVitals.tsx";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
