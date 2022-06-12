1e4fb17e308e3829285d793ab1439a12549b7638
logic-fabric <lowic@logic-fabric.fr>
2022-06-12 18:55:07 +0200

    style: TodoListItem

--- src/molecules/TodoListItem/TodoListItem.tsx
+++ src/molecules/TodoListItem/TodoListItem.tsx
@@ -1,18 +1,51 @@
 import { useState } from "react";
 
+import styled from "styled-components";
+
 import { NewTaskButton } from "../../atoms/NewTaskButton/NewTaskButton.tsx";
 
 export function TodoListItem(): JSX.Element {
   const [canInputTask, setCanInputTask] = useState(false);
 
   return (
-    <li>
-      {canInputTask && <input type="text" />}
+    <ListItem>
+      {canInputTask && <TextInput />}
 
-      <NewTaskButton 
+      <NewTaskButton
         canInputTask={canInputTask}
         onClick={(): void => setCanInputTask(!canInputTask)}
       />
-    </li>
+    </ListItem>
   );
 }
+
+const ListItem = styled.li`
+  padding: 0.75rem;
+  border-radius: 0.5rem;
+
+  background: var(--primary-100);
+
+  transition: all 300ms;
+`;
+
+const TextInput = styled.input`
+  margin: 0 1rem 0 0.125rem;
+  padding: 0.75rem 0;
+  border: 0 solid transparent;
+  border-width: 0.25rem 0;
+  border-bottom-color: var(--primary-500);
+  overflow: hidden;
+
+  color: var(--primary-700);
+  font-size: var(--txt-size);
+  font-weight: 500;
+
+  background: var(--primary-100);
+  outline: none;
+
+  transition: all 300ms;
+
+  &:focus {
+    border-color: var(--primary-500);
+  }
+`;
--- src/pages/Home/HomePage.tsx
+++ src/pages/Home/HomePage.tsx
@@ -1,11 +1,17 @@
+import styled from "styled-components";
+
 import { TodoList } from "../../organisms/TodoList/TodoList.tsx";
 
 export function HomePage(): JSX.Element {
   return (
     <main>
-      <h1>React/TypeScript TODO List</h1>
+      <MainTitle>React/TypeScript TODO List</MainTitle>
 
       <TodoList />
     </main>
   );
 }
+
+const MainTitle = styled.h1`
+  text-align: center;
+`;
