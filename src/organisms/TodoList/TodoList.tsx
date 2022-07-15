64122db6e0fbf11c27100f1a81184b4d8196925a
logic-fabric <lowic@logic-fabric.fr>
2022-07-15 09:47:40 +0200

    feature: add a todoTasks state to TodoList, using a ITodoTask interface

--- src/organisms/TodoList/TodoList.tsx
+++ src/organisms/TodoList/TodoList.tsx
@@ -1,8 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 
 import { TodoListItem } from "../../molecules/TodoListItem/TodoListItem.tsx";
 
+interface ITodoTask {
+  description: string;
+  isDone: boolean;
+}
+
 export function TodoList(): JSX.Element {
+  const [todoTasks, setTodoTasks] = useState<ITodoTask[]>([]);
+
   return (
     <ol>
       <TodoListItem />
